c75440af3f414d583ae607ae1de6cfebba27ab95
Stefan Angelov <cefothe@gmail.com>
2016-02-27 00:24:18 +0200

    Initialize repository

---
+++ Example_1/src/Example_1_OOP.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Example_1_OOP.cpp
+// Author      : Stefan Angelov(cefothe)
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
